e929de865ed235b036cdd1a7ad4dde2e9de14b8a
brawnydawg <brawnydog@icloud.com>
2022-08-01 16:44:04 -0400

    game 1 init

---
+++ game1/main.asm
@@ -0,0 +1,15 @@
+BasicUpstart2(start)  // 10 sys$0810
+
+start: 
+    sei         //set interrupt disable flag
+
+
+// ============================================================
+// Initialize the screen
+// ============================================================
+init_screen:
+
+
+
+
+
